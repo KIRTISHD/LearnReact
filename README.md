e0ce5f9abf941d8a32bd20c7190eed6d6c0089f3
KIRTISHD <aniket.dhande@gmail.com>
2022-01-03 12:23:25 +0530

    create and fetch from json

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Branch b3 - useState react hook; passing data from parent to child and vice vers
 Branch b4 - persisting data using localstorage; delete contact
 Branch b5 - React Router intro, normal use
 Branch b6 - New Contact Detail page, passing info across components.
+Branch b7 - Install axios, create and fetch data from json.
--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,25 @@ import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 import ContactDetail from './ContactDetail';
+import api from '../api/contacts';
 
 function App() {
-  const LOCAL_STORAGE_KEY = "contacts";
   // [] in useState indicates initial value for contacts array
   const [contacts, setContacts] = useState([]);
+  
+   const retrievedContacts = async() => {
+     const response = await api.get("/contacts");
+     return response.data;
+   };
 
-
-  const addContactHandler = (contact) => {
+  const addContactHandler = async(contact) => {
     //use a function to add object in array and not directly to array
-    setContacts([...contacts, {id: uuidv4(), ...contact}]);
+    const request = {
+      id: uuidv4(),
+      ...contact
+    };
+    const response = await api.post("/contacts", request);
+    setContacts([...contacts, response.data]);
   }
 
   const removeContactHandler = (id) => {
@@ -27,12 +36,18 @@ function App() {
   }
 
   useEffect(() => {
-    const retrievedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (retrievedContacts) setContacts(retrievedContacts);
+    // const retrievedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    // if (retrievedContacts) setContacts(retrievedContacts);
+    const getAllContacts = async() => {
+      const AllContacts = await retrievedContacts();
+      if (AllContacts) setContacts(AllContacts);
+    };
+
+    getAllContacts();
   }, []);
 
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts));
+    //localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts));
   }, [contacts]);
 
   return (
